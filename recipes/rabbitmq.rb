59867fc137d838167d015942429fad17474ee149
Gavin M. Roy <gavinmroy@gmail.com>
2014-02-18 00:02:25 -0500

    Go low-tech for the RabbitMQ cookie

--- recipes/rabbitmq.rb
+++ recipes/rabbitmq.rb
@@ -4,18 +4,7 @@
 #
 # Copyright 2013 Manning Publications
 #
-service_name = node[:rabbitmq][:service_name]
-
-file '/var/lib/rabbitmq/.erlang.cookie' do
-    action  :nothing
-    content "XBCDDYAVPRVEYREVJLXS\n"
-    owner 'rabbitmq'
-    group 'rabbitmq'
-    mode  0400
-    notifies :start, "service[#{service_name}]", :immediate
+execute 'update-cookie' do
+    command 'service rabbitmq-server stop; echo "XBCDDYAVPRVEYREVJLXS" > /var/lib/rabbitmq/.erlang.cookie; service rabbitmq-server start'
 end
 
-service node[:rabbitmq][:service_name] do
-    action :stop
-    notifies :create, "file[/var/lib/rabbitmq/.erlang.cookie]", :immediate
-end
