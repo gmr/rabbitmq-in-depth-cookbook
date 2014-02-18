2f5ce887ecb18e2b102f71251ad60982260286e3
Gavin M. Roy <gavinmroy@gmail.com>
2014-02-17 22:32:51 -0500

    Make sure that aptitude is up to date

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,11 @@
 # Copyright 2013 Manning Publications
 #
 
+# Make sure apt caches, etc are up to date
+execute 'aptitude-update' do
+    command 'apt-get update'
+end
+
 # Ensure the libraries used in the Python examples are always up to date
 %w[pika pamqp rabbitpy mosquitto stomp.py].each do |pkg|
   python_pip pkg do
