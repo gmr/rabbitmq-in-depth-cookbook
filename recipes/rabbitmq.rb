43d0d93ae94fa1c15810fac57c65977d8a52663d
Gavin M. Roy <gavinmroy@gmail.com>
2014-02-17 23:28:53 -0500

    Remove the plugin installation bits and the include

--- recipes/rabbitmq.rb
+++ recipes/rabbitmq.rb
@@ -1,11 +1,9 @@
 #
 # Cookbook Name:: rabbitmq-in-depth
-# Recipe:: ipython
+# Recipe:: rabbitmq
 #
 # Copyright 2013 Manning Publications
 #
-include_recipe "rabbitmq::default"
-
 service_name = node[:rabbitmq][:service_name]
 
 file '/var/lib/rabbitmq/.erlang.cookie' do
@@ -20,24 +18,4 @@ end
 service node[:rabbitmq][:service_name] do
     action :stop
     notifies :create, "file[/var/lib/rabbitmq/.erlang.cookie]", :immediate
-end
-
-plugins = %w[rabbitmq_management
-             rabbitmq_management_visualiser
-             rabbitmq_consistent_hash_exchange
-             rabbitmq_federation
-             rabbitmq_federation_management
-             rabbitmq_mqtt
-             rabbitmq_shovel
-             rabbitmq_shovel_management
-             rabbitmq_stomp
-             rabbitmq_tracing
-             rabbitmq_web_stomp
-             rabbitmq_web_stomp_examples]
-
-plugins.each do |plugin|
-  rabbitmq_plugin plugin do
-    action :enable
-    notifies :restart, "service[#{service_name}]", :delayed
-  end
 end
